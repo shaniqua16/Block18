c2bf29a3c5fc23e17e549648744d2355eab1d4bc
Shaniqua <shaniquawhitley3@gmail.com>
2025-04-18 09:06:20 -0500

    1st comm

---
+++ test.js
@@ -0,0 +1,43 @@
+          //unit tests for multiplication //
+
+// Expect multiplication(2, 3) to be a number
+// Expect multiplication(2, 3) to be equal to 6
+// Expect multiplication(5, 10) to be equal to 50
+
+// Expect multiplication(10, 0) to be equal to 0
+// Expect multiplication(0, 5) to be equal to 0
+// Expect multiplication(0, 0) to be equal to 0
+// Expect multiplication(-2, 3) to be equal to -6
+// Expect multiplication(2, -3) to be equal to -6
+// Expect multiplication(-2, -3) to be equal to 6
+
+// Expect multiplication("a", 3) to be an error (or throw an error)
+// Expect multiplication(3, "b") to be an error
+// Expect multiplication("a", "b") to be an error
+// Expect multiplication(null, 5) to be an error
+// Expect multiplication(5, undefined) to be an error
+// Expect multiplication([1, 2], 3) to be an error
+// Expect multiplication(3, {num: 2}) to be an error
+// Expect multiplication(4) to be an error 
+
+        // unit tests for concatOdds //
+// Expect concatOdds([], []) to be equal to []
+// Expect concatOdds([1, 3, 5], []) to be equal to [1, 3, 5]
+// Expect concatOdds([], [9, 7, 11]) to be equal to [7, 9, 11] 
+// Expect concatOdds([2, 4, 6], [8, 10]) to be equal to []
+// Expect concatOdds([1, 5], [3, 7]) to be equal to [1, 3, 5, 7]
+// Expect concatOdds([-3, 2, 5], [0, -1, 8]) to be equal to [-3, -1, 5] (Testing negative odds and evens)
+// Expect concatOdds([3, 3, 3], [1, 1]) to be equal to [1, 3]
+
+           // functions for checkout process //
+// Expect clicking the 'Cart' icon/link displays the items currently in the cart and their total price.
+// Expect clicking the 'Proceed to Checkout' button (when the cart is not empty) navigates the user to the first step of the checkout process.
+// Expect a logged-in user to see their default shipping address pre-filled.
+// Expect a guest user to be presented with a form to enter their shipping address.
+// Expect the user to be able to proceed from Shipping Information to Payment Information.
+// Expect a logged-in user to see their saved payment methods.
+// Expect the Order Review page to display the items, selected shipping address, selected payment method, and final total cost.
+// Expect clicking the 'Place Order' button successfully processes the order and navigates the user to an Order Confirmation page.
+// Expect the Order Confirmation page to display a success message and an order summary (like an order number).
+
+
